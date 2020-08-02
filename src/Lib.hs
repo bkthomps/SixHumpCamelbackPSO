60fbb0ef74591c1856393bec06539ce0c7c73180
bkthomps <bkthomps@uwaterloo.ca>
2020-08-02 03:39:54 -0400

    Add camelback function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,12 @@
 module Lib
-  ( someFunc,
+  ( simplePso,
   )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+simplePso :: IO ()
+simplePso = print (sixHumpCamelback 4 2)
+
+sixHumpCamelback :: Double -> Double -> Double
+sixHumpCamelback x y
+  | max (abs x) (abs y) > 5 = error "Bad camelback range"
+  | otherwise = (4 - 2.1 * x ^ 2 + (1 / 3) * x ^ 4) * x ^ 2 + x * y + ((-4) + 4 * y ^ 2) * y ^ 2
