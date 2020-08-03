50d2178a68365cce3f6b966761c3a01111a90b91
bkthomps <bkthomps@uwaterloo.ca>
2020-08-03 14:32:39 -0400

    Add file output

--- app/Main.hs
+++ app/Main.hs
@@ -6,25 +6,23 @@ import System.Random (newStdGen, randomRs)
 main :: IO ()
 main = do
   list <- randomList
-  let (x_1, y_1, best_1, avg_1) = simplePso list
-  print ((show x_1) ++ " " ++ (show y_1))
-  print (show best_1)
-  print (show avg_1)
-  print ""
-  let (x_2, y_2, best_2, avg_2) = inertiaWeightPso list
-  print ((show x_2) ++ " " ++ (show y_2))
-  print (show best_2)
-  print (show avg_2)
-  print ""
-  let (x_3, y_3, best_3, avg_3) = constrictionCoefficientPso list
-  print ((show x_3) ++ " " ++ (show y_3))
-  print (show best_3)
-  print (show avg_3)
-  print ""
-  let (x_4, y_4, best_4, avg_4) = guaranteedConvergencePso list
-  print ((show x_4) ++ " " ++ (show y_4))
-  print (show best_4)
-  print (show avg_4)
+  writePso "1_simple_pso" (simplePso list)
+  writePso "2_inertia_weight_pso" (inertiaWeightPso list)
+  writePso "3_constrict_coefficient_pso" (constrictionCoefficientPso list)
+  writePso "4_guaranteed_convergence_pso" (simplePso list)
 
 randomList :: IO [Double]
 randomList = randomRs (0.0, 1.0) <$> newStdGen
+
+writePso :: String -> (Double, Double, [Double], [Double]) -> IO ()
+writePso name (x, y, best, avg) = do
+  writeFile (name ++ "_solution.csv") (displayPair x y)
+  writeFile (name ++ "_best.csv") ("sep=,\n" ++ displayList best)
+  writeFile (name ++ "_average.csv") ("sep=,\n" ++ displayList avg)
+
+displayPair :: Double -> Double -> String
+displayPair x y = "(" ++ show x ++ ", " ++ show y ++ ")\n"
+
+displayList :: [Double] -> String
+displayList (first : list) = show first ++ "\n" ++ displayList(list)
+displayList _ = []
